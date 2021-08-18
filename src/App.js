64ee36722904d01ea555d86f181f3033e619aaa7
samstorment <sjstorment22@gmail.com>
2021-08-18 14:14:38 -0500

    Elecdrawn descriptions done

--- src/App.js
+++ src/App.js
@@ -10,18 +10,19 @@ import { ProjectList } from 'components/project-list/ProjectList';
 import { Project } from 'components/project/Project';
 
 export const App = () => {
+
 	return <div id="app">
 		<Router>
 			<Header />
 			<main>
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/contact" component={Contact} />
-						<Route exact path="/resume" component={Resume} />
-						<Route exact path="/projects" component={ProjectList} />
-						<Route exact path="/projects/:projectName" component={Project} />
-						<Route path="/" component={FourOFour} />
-					</Switch>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/contact" component={Contact} />
+					<Route exact path="/resume" component={Resume} />
+					<Route exact path="/projects" component={ProjectList} />
+					<Route exact path="/projects/:projectName" component={Project} />
+					<Route path="/" component={FourOFour} />
+				</Switch>
 			</main>
 			<Footer />
 		</Router>
--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,8 +1,39 @@
-import { useParams } from "react-router-dom"
+import projects from 'data/project-list.json';
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Headline } from "components/reusable/headline/Headline";
+import { FourOFour } from "components/FourOFour/FourOFour";
+import './project.css';
 
 export const Project = () => {
 
     const { projectName } = useParams();
+    const [ project, setProject ] = useState(null);
+    const [ showButtons, setShowButtons ] = useState(false);
+ 
+    useEffect(() => {
+        const [ proj ] = projects.filter(p => p.id === projectName);
+        setProject(proj);
+        setShowButtons(!!proj.github || !!proj.website);
+    }, [projectName]);
 
-    return <div style={{textAlign: "center"}}>{projectName}</div>
+    if (!project) return <FourOFour />;
+
+    return <article>
+        {/* <h4 className="article-title">{project.title}</h4> */}
+        <Headline>{project.title.toUpperCase()}</Headline>
+        {showButtons && <div className="project-button-container">
+            {project.github && <a href={project.github} className="project-button" target="_blank" rel="noopener noreferrer">GitHub</a>}
+            {project.website && <a href={project.website} className="project-button" target="_blank" rel="noopener noreferrer">Live Demo</a>}
+        </div>}
+        {project.images && project.images.map(img => {
+            return <>
+                <h4 className="article-title">{img.title}</h4>
+                <div className="project-image-container">
+                    <img className="project-image" src={img.path} alt={img.title} />
+                </div>
+                <p style={{margin: "5px 0px 20px 0px"}}>{img.description}</p>
+            </>
+        })}
+    </article>
 }
